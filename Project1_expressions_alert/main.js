fec6b5ed80491cce656f53a0b9845f4a3e63a07b
AlwaysOnCue47 <martin.fobert@gmail.com>
2024-10-16 14:15:53 -0400

    created html and js files
    added new files for JavaScript course. index.html in Javascript project folder and corresponding js file

---
+++ Project1_expressions_alert/main.js
@@ -0,0 +1,10 @@
+document.write("hello world!"); //prints the text on the body of the html - i think
+window.alert("Hello, World!"); // prints the message in an alert box
+console.log("Log this");
+var A = " \"This is a string.\" Said, Sandy." + " \"Isn\'t that special.\" Said, Jerry. ";
+document.write(A);
+var family="The SImpsons", dad="Homer", mom="Marge", son="Bart", daughter="Lisa"; //assign multiple varibles 
+document.write(son);
+console.log(mom, son); // prints the message in the console log
+document.write(3+3);
+console.log(3+3);
