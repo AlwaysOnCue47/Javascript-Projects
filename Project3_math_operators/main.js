d1a363cf966931cb46560d674d7f7ee5616867b7
AlwaysOnCue47 <martin.fobert@gmail.com>
2024-10-17 17:10:53 -0400

    Module 2 math operations
    project 3 - math operations.

--- Project3_math_operators/main.js
+++ Project3_math_operators/main.js
@@ -1,4 +1,35 @@
-function multiply(x, y) {
+function Multiply(x, y) {
   result =  x * y;
-  document.getElementById("Math").innerHTML = "Answer is: " + result;
+  document.getElementById("multiply").innerHTML = "Answer is: " + result;
+}
+
+function Subtract(x, y) {
+  result =  x - y;
+  document.getElementById("subtract").innerHTML = "Answer is: " + result;
+}
+
+function Addition(x, y) {
+  result =  x + y;
+  document.getElementById("addition").innerHTML = "Answer is: " + result;
+}
+
+function Division(x, y) {
+  result =  x / y;
+  result = result.toFixed(2); // rounds result to max 2 decimal places
+  document.getElementById("division").innerHTML = "Answer is: " + result;
+}
+
+function Math(x, y, z, a) {
+  result = (x - y)*(z + a);
+  document.getElementById("math").innerHTML = "Answer is: " + result;
+}
+
+function Modulus(x, y) {
+  result = x % y;
+  document.getElementById("modulus").innerHTML = "Answer is: " + result;
+}
+
+function Negation(x) {
+  result = -x ;
+  document.getElementById("negation").innerHTML = "Answer is: " + result;
 }
