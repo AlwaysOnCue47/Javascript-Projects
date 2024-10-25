4c3a944497ae9c5d1c016275b7d3ad76baa630d4
AlwaysOnCue47 <martin.fobert@gmail.com>
2024-10-25 15:29:33 -0400

    module 4
    module 4 canvas assignment

--- Module_4/js/main.js
+++ Module_4/js/main.js
@@ -6,3 +6,10 @@ var ctx = myCanvas.getContext("2d");
 ctx.moveTo(0,0);
 ctx.lineTo(200,100);
 ctx.stroke();
+
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(20,20,200,200);
