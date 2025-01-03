6082c5be7c2454b35957acba13b7739f903ac9f2
Home_System <martin.fobert@gmail.com>
2025-01-03 18:33:30 -0500

    Update canvas.js
    making circles with canvas

--- test_folder/Canvas/canvas.js
+++ test_folder/Canvas/canvas.js
@@ -1,27 +1,13 @@
 // canvas javascript
 
 // variables
-const canvas = document.getElementById('canvas1');
-canvas.height = document.documentElement.clientHeight;
-canvas.width = document.documentElement.clientWidth;
-let c = canvas.getContext('2d');
+const canvas = document.getElementById('canvas1'); // the canvas
+canvas.height = document.documentElement.clientHeight; // canvas height
+canvas.width = document.documentElement.clientWidth; // canvas width
+let c = canvas.getContext('2d'); // canvas' "paint-brush"
 
-// logic
-
-
-/*c.fillStyle = 'rgba(255, 255, 255, 0.5)';
-c.fillRect(100, 100, 100, 100);
-
-c.fillStyle = 'rgba(160, 200, 150, 0.8)';
-c.fillRect(300, 50, 100, 100);
-
-c.beginPath();
-c.moveTo(50, 400);
-c.lineTo(100, 500);
-c.lineTo(160, 300);
-c.strokeStyle = "rgb(29, 26, 196)";
-c.stroke();*/
-
+/*
+// Circle animation
 
 X = Math.random() * canvas.width;
 Y = Math.random() * canvas.height;
@@ -48,10 +34,73 @@ function animate() {
   Y += DY;
  
 };
+*/
 
-animate();
+// circle object constructure
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+  }
+
+  this.update = function() {
+    if (this.x + this.radius >= canvas.width || this.x - this.radius <=  0) {
+      this.dx = -this.dx;
+    }
+    
+    if (this.y + this.radius >= canvas.height || this.y - this.radius <= 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+
+  }
+};
+
+var circle1 = new Circle(200, 200, 4, 4, 40);
+
+let circleArray = [];
+
+for (let  i = 0;  i < 50; i++) {
+  circleRadius = 40;
+  X = Math.floor(Math.random() * (canvas.width - circleRadius * 2) + circleRadius + 1);
+  Y = Math.floor(Math.random() * (canvas.height - circleRadius * 2) + circleRadius + 1);
+  DX = Math.random() * 6;
+  DY = Math.random() * 6;
   
-/*for (i = 0; i < 50; i++) {
+  circleArray.push(new Circle(X, Y, DX, DY, circleRadius));
+  
+}
+
+console.log(circleArray);
+
+function animate2() {
+  requestAnimationFrame(animate2);
+  c.clearRect(0, 0, canvas.width, canvas.height); 
+  
+  for (let i = 0; i < circleArray.length; i++){
+    circleArray[i].update();
+  }
+};
+
+animate2();
+
+
+
+/* Random circle generator
+
+for (i = 0; i < 50; i++) {
 Y = Math.random()* window.innerHeight;
 X = Math.random()* window.innerWidth;
 randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -61,5 +110,20 @@ c.arc(X, Y, 30, Math.PI * 2, false);
 c.strokeStyle = randomColor;
 c.stroke();
 };*/
+
+/* Rectangle drawer
+
+c.fillStyle = 'rgba(255, 255, 255, 0.5)';
+c.fillRect(100, 100, 100, 100);
+
+c.fillStyle = 'rgba(160, 200, 150, 0.8)';
+c.fillRect(300, 50, 100, 100);
+
+c.beginPath();
+c.moveTo(50, 400);
+c.lineTo(100, 500);
+c.lineTo(160, 300);
+c.strokeStyle = "rgb(29, 26, 196)";
+c.stroke();*/
 
 // event listeners
