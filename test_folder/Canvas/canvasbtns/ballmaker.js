65276221fdd3559343ffae0efd75b7083f92236d
Home_System <martin.fobert@gmail.com>
2025-01-15 23:22:33 -0500

    playing around with animations and collisions

--- test_folder/Canvas/canvasbtns/ballmaker.js
+++ test_folder/Canvas/canvasbtns/ballmaker.js
@@ -1,10 +1,12 @@
-
+// the canvas 
 const canvas = document.getElementById('canvas');
 
 canvas.height = document.documentElement.clientHeight;
 canvas.width = document.documentElement.clientWidth;
 
 const ctx = canvas.getContext('2d');
+
+// variables
 
 let isAnimating = false;
 
@@ -14,6 +16,11 @@ document.addEventListener('mousemove', (event)=> {
   mouse.y = event.y;
 });
 
+let blackBall = new Ball(200, 300, 30, 'black');
+let balls;
+
+// event listeners 
+
 document.getElementById('popBtn').addEventListener('click', () => {
   if (!isAnimating){
     init();
@@ -21,8 +28,8 @@ document.getElementById('popBtn').addEventListener('click', () => {
     isAnimating = true;
     console.log(isAnimating);
     console.log(balls);
+
   }
-  
 } 
 );
 
@@ -42,20 +49,32 @@ document.getElementById('break').addEventListener('click', () => {
   console.log(balls);
 });
 
-function getDistance(x1, y1, x2, y2){
-  let xDistance = x2 - x1;
-  let yDistance = y2 - y1;
+document.getElementById('shoot').addEventListener('click', () => {
+  if (!fired){ 
+    shootCannon();
+    fired = true;
+  }
+  
+  
+});
 
-  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+document.getElementById('reload').addEventListener('click', () => {
+  loadCannon();
 
-};
+});
+
+// object constructor 
 
 function Ball(x, y, radius, color, vx, vy) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.color = color;
-  this.velocity = {x:vx, y:vy};
+  this.velocity = {
+    x: vx,
+    y: vy
+  };
+  this.mass = 1;
 
   this.update = () => {
     if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
@@ -72,7 +91,7 @@ function Ball(x, y, radius, color, vx, vy) {
 
   };
 
-  this.draw = ()=>{
+  this.draw = () => {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
     ctx.fillStyle = this.color;
@@ -82,10 +101,64 @@ function Ball(x, y, radius, color, vx, vy) {
   };
 };
 
-let blackBall = new Ball(200, 300, 30, 'black');
-blackBall.draw();
-let balls;
+function Cannon(vx, vy) {
+  this.x = 300 //canvas.width - 5;
+  this.y =  300 //canvas.height - 5;
+  this.radius = 10;
+  this.color = 'black';
+  this.velocity = {x:vx, y:vy};
+  this.gravity = 0.5;
+  this.mass = 1;
 
+  this.draw = () => {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+
+  };
+
+  this.update = ()=> {
+    this.velocity.y += this.gravity;
+    this.y += this.velocity.y;
+    this.x += this.velocity.x;
+    this.draw();
+
+  }
+
+}
+
+// functions 
+let cannonBall;
+let fired = false;
+let fire;
+
+function loadCannon() {
+  if (fired) {
+    cancelAnimationFrame(fire);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fired = false;
+  } 
+    cannonBall = new Cannon(15, -15);
+    cannonBall.draw();
+    console.log(cannonBall);
+  
+}
+
+function shootCannon() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  fire = requestAnimationFrame(shootCannon);
+  cannonBall.update();
+};
+
+function getDistance(x1, y1, x2, y2) {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+
+};
 
 function init() {
   balls = [];
@@ -94,9 +167,8 @@ function init() {
     let y = (Math.random()* (canvas.height - 60)) +30;
     balls.push(new Ball(x, y, 30, 'red', 1, 2));
     balls[i].draw();
-    
+
   }
-  
 };
 
 function animate() {
@@ -104,13 +176,20 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     blackBall.x = mouse.x;
     blackBall.y = mouse.y;
-    blackBall.draw()
+    blackBall.draw();
+
     for (let i = 0; i < balls.length; i++) {
       if (getDistance(blackBall.x, blackBall.y, balls[i].x, balls[i].y) - blackBall.radius * 2 < 0) {
         balls[i].color = 'blue';
       }
       balls[i].update();
-    };
 
+    };
   } ;
-    
+
+
+// Run when parsed 
+  loadCannon();
+  //shootCannon();
+  
+  blackBall.draw();
