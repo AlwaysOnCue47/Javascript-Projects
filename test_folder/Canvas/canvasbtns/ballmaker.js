323498b932da3855c6d46d9a1b43b8ca1f4beecb
Home_System <martin.fobert@gmail.com>
2025-01-15 21:28:32 -0500

    experimenting with collision detention. Ballmaker.js

--- test_folder/Canvas/canvasbtns/ballmaker.js
+++ test_folder/Canvas/canvasbtns/ballmaker.js
@@ -6,27 +6,66 @@ canvas.width = document.documentElement.clientWidth;
 
 const ctx = canvas.getContext('2d');
 
+let isAnimating = false;
+
+let mouse = {x:undefined, y: undefined};
+document.addEventListener('mousemove', (event)=> {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
 document.getElementById('popBtn').addEventListener('click', () => {
-  init();
-  animate();
+  if (!isAnimating){
+    init();
+    animate();
+    isAnimating = true;
+    console.log(isAnimating);
+    console.log(balls);
+  }
+  
 } 
 );
 
 document.getElementById('clearScr').addEventListener('click', ()=> {
-  window.cancelAnimationFrame(reAnim);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (isAnimating){
+    window.cancelAnimationFrame(reAnim);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    isAnimating = false;
+    console.log(isAnimating);
+    console.log(balls);
+  }
 }
 );
 
+document.getElementById('break').addEventListener('click', () => {
+  balls.splice(0, 1);
+  console.log(balls);
+});
 
-function Ball(x, y, radius, color) {
+function getDistance(x1, y1, x2, y2){
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+
+};
+
+function Ball(x, y, radius, color, vx, vy) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.color = color;
-  this.velocity = {x:1, y:2};
+  this.velocity = {x:vx, y:vy};
 
   this.update = () => {
+    if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
+      this.velocity.x = -this.velocity.x;
+    }
+
+    if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height) {
+      this.velocity.y = -this.velocity.y;
+    }
+
     this.x += this.velocity.x;
     this.y += this.velocity.y;
     this.draw();
@@ -42,16 +81,36 @@ function Ball(x, y, radius, color) {
 
   };
 };
-let ball1;
+
+let blackBall = new Ball(200, 300, 30, 'black');
+blackBall.draw();
+let balls;
 
 
 function init() {
-  ball1 = new Ball(100, 100, 30, 'red');
-  ball1.draw();
-}
+  balls = [];
+  for (let i = 0; i < 10; i++) {
+    let x = (Math.random()* (canvas.width - 60)) +30;
+    let y = (Math.random()* (canvas.height - 60)) +30;
+    balls.push(new Ball(x, y, 30, 'red', 1, 2));
+    balls[i].draw();
+    
+  }
+  
+};
 
 function animate() {
-  reAnim = requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ball1.update();
-};
+    reAnim = requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    blackBall.x = mouse.x;
+    blackBall.y = mouse.y;
+    blackBall.draw()
+    for (let i = 0; i < balls.length; i++) {
+      if (getDistance(blackBall.x, blackBall.y, balls[i].x, balls[i].y) - blackBall.radius * 2 < 0) {
+        balls[i].color = 'blue';
+      }
+      balls[i].update();
+    };
+
+  } ;
+    
