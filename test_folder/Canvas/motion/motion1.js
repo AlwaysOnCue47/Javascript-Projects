bccaee9378b8f9d5144eec71a3b4915ea7004e1a
Home_System <martin.fobert@gmail.com>
2025-01-27 12:44:41 -0500

    controller object on canvas with keypress event listeners

--- test_folder/Canvas/motion/motion1.js
+++ test_folder/Canvas/motion/motion1.js
@@ -4,21 +4,31 @@ const ctx = canvas.getContext('2d');
 
 
 // variables
+const spriteRadius = 8;
 
 // event listeners
  addEventListener('keydown', (event)=> {
   switch (event.key) {
     case 'ArrowUp':
-      sprite1.vy = 1;
+      sprite1.vy = -1;
+      sprite1.vx = 0;
       break;
     case 'ArrowDown':
-      sprite1.vy = -1;
+      sprite1.vy = 1;
+      sprite1.vx = 0;
       break;
     case 'ArrowLeft':
       sprite1.vx = -1;
+      sprite1.vy = 0;
       break;
     case 'ArrowRight':
       sprite1.vx = 1;
+      sprite1.vy = 0;
+      break;
+    case ' ':
+      sprite1.vx = 0;
+      sprite1.vy = 0;
+      break;
 
   };
      
@@ -38,6 +48,7 @@ class Sprite {
   }
 
   draw() {
+    ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
     ctx.fill();
@@ -45,6 +56,18 @@ class Sprite {
   }
 
   update() {
+    if (this.x - spriteRadius <= 0) {
+      this.x = spriteRadius;
+    }
+    if (this.x + spriteRadius >= canvas.width) {
+      this.x = canvas.width - spriteRadius;
+    }
+    if (this.y - spriteRadius <= 0) {
+      this.y = spriteRadius;
+    }
+    if (this.y + spriteRadius >= canvas.height){
+      this.y = canvas.height - spriteRadius;
+    }
     this.x += this.vx;
     this.y += this.vy;
     this.draw();
@@ -52,11 +75,12 @@ class Sprite {
   }
 };
 
-const sprite1 = new Sprite(60, 60, 8, 'black', 0, 0);
+const sprite1 = new Sprite(60, 60, spriteRadius, 'black', 0, 0);
 // functions
 
 function animate(){
   runAnim = requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   sprite1.update();
 }
 
