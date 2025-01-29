6d28a46f5011b0a30d52262a212cdaf660214b6e
Home_System <martin.fobert@gmail.com>
2025-01-29 17:56:16 -0500

    experimenting with sprites in canvas

--- test_folder/Canvas/motion/motion1.js
+++ test_folder/Canvas/motion/motion1.js
@@ -1,28 +1,32 @@
 // canvas
 const canvas = document.getElementById('canvas');
+canvas.height = 420;
+canvas.width = 680;
+canvas.style.width = 680;
+canvas.style.height = 420;
 const ctx = canvas.getContext('2d');
 
 
 // variables
-const spriteRadius = 8;
+const spriteRadius = 12;
 
 // event listeners
  addEventListener('keydown', (event)=> {
   switch (event.key) {
     case 'ArrowUp':
-      sprite1.vy = -1;
+      sprite1.vy = -4;
       sprite1.vx = 0;
       break;
     case 'ArrowDown':
-      sprite1.vy = 1;
+      sprite1.vy = 4;
       sprite1.vx = 0;
       break;
     case 'ArrowLeft':
-      sprite1.vx = -1;
+      sprite1.vx = -4;
       sprite1.vy = 0;
       break;
     case 'ArrowRight':
-      sprite1.vx = 1;
+      sprite1.vx = 4;
       sprite1.vy = 0;
       break;
     case ' ':
@@ -58,6 +62,7 @@ class Sprite {
   update() {
     if (this.x - spriteRadius <= 0) {
       this.x = spriteRadius;
+      
     }
     if (this.x + spriteRadius >= canvas.width) {
       this.x = canvas.width - spriteRadius;
@@ -78,14 +83,34 @@ class Sprite {
 const sprite1 = new Sprite(60, 60, spriteRadius, 'black', 0, 0);
 // functions
 
+let pillArray = []
+
+function initPills() {
+  for (let i = 0; i < 8; i++) {
+    let x = (Math.random()*canvas.width - 20) + 20;
+    let y = (Math.random()*canvas.height - 20) + 20;
+    let pillRadius = 6;
+    pillArray.push(new Sprite(x, y, pillRadius, 'red', 0, 0));
+    pillArray[i].draw();
+    
+  }
+};
+
 function animate(){
   runAnim = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   sprite1.update();
-}
+  for (let i = 0; i < pillArray.length; i++) {
+    pillArray[i].update();
+    
+  }
+};
 
 // run when parsed
 sprite1.draw();
-animate();
+initPills();
+ animate();
 console.log(sprite1);
-
+console.log(canvas.width);
+console.log(canvas.height);
+console.log(window.devicePixelRatio);
