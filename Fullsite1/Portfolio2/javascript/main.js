3dc9b4b7053d187bcb49397dd3d909f246a067c6
Home_System <martin.fobert@gmail.com>
2025-03-27 15:26:00 -0400

    working on portfolio page - getting there

--- Fullsite1/Portfolio2/javascript/main.js
+++ Fullsite1/Portfolio2/javascript/main.js
@@ -37,7 +37,8 @@ function showSlides(n) {
 function getDistance(x1, y1, x2, y2) {
   let xDistance = x2 - x1;
   let yDistance = y2 - y1;
-  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  return Math.sqrt(Math.pow(xDistance, 2) 
+       + Math.pow(yDistance, 2));
 };
 
 // CANVAS 1 DEMO
