65d707b9aba34e19d6ed9194c0b112b2c913cb50
Home_System <martin.fobert@gmail.com>
2025-04-30 15:47:04 -0400

    working on new project - game 2

--- PacManClone/pacman.cs
+++
@@ -1,1 +0,0 @@
-/*  Styles for Pacman Clone game */
--- PacManClone/pacman.js
+++ PacManClone/pacman.js
@@ -1,3 +1,10 @@
 // Script for Pacman Clone Game
 
 // The Canvas
+
+const canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.background = 'black';
