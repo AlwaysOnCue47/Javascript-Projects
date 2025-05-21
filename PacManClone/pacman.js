cf8dfc2b340a8b374dcd09f53fbea01d0b09def4
Home_System <martin.fobert@gmail.com>
2025-05-21 15:40:29 -0400

    Update pacman.js
    started pacman clone game. Created boundary constructor.

--- PacManClone/pacman.js
+++ PacManClone/pacman.js
@@ -8,3 +8,69 @@ ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.style.background = 'black';
+
+// constructors 
+
+class Boundary {
+  static width = 40;
+  static height = 40;
+  constructor({position}) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+};
+
+
+
+// variables
+
+const map = [
+  ['-', '-', '-', '-', '-', '-'],
+  ['-', ' ', ' ', ' ', ' ', '-'],
+  ['-', ' ', '-', '-', ' ', '-'],
+  ['-', ' ', ' ', ' ', ' ', '-'],
+  ['-', '-', '-', '-', '-', '-']
+];
+
+const bounderies = [];
+
+map.forEach((row, i) =>{
+  row.forEach((symbol, j) => {
+    switch (symbol){
+      case '-':
+        bounderies.push(
+          new Boundary({
+            position: {
+              x: Boundary.width * j,
+              y: Boundary.height * i
+            }
+          })
+        )
+        break;
+      case ' ':
+        break;
+      
+    }
+  })
+
+})
+
+
+// const boundary = new Boundary({
+//   position:{
+//     x:0, 
+//     y:0
+//   }
+// });
+
+// boundary.draw();
+
+bounderies.forEach((boundary) => {
+  boundary.draw()
+});
